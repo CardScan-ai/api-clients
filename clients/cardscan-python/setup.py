621b9ae85a6e53015279fab0985c2fdc653056fe
Christian Rivera <christian.rivera@inowu.dev>
2024-05-27 18:59:03 -0700

    chore(python): bump minor version

--- clients/cardscan-python/setup.py
+++ clients/cardscan-python/setup.py
@@ -9,14 +9,14 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "cardscan-client"
-VERSION = "0.0.1"
+VERSION = "0.1.0"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
     "python-dateutil",
     "pydantic >= 1.10.5, < 2",
     "aenum",
-    "websockets"
+    "websockets",
 ]
 
 setup(
@@ -30,7 +30,7 @@ setup(
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     long_description="""\
     The official python client for the CardScan API.
     """,  # noqa: E501
