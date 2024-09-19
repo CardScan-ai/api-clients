94e6cd28f57917df6650bda4276f8270884b4a28
Christian Rivera <christian.rivera@inowu.dev>
2024-09-19 15:47:23 -0700

    chore: re-generate client

--- clients/cardscan-ts/api/card-scan-api.ts
+++ clients/cardscan-ts/api/card-scan-api.ts
@@ -694,56 +694,6 @@ export const CardScanApiAxiosParamCreator = function (
     },
     /**
      *
-     * @summary Get Scan Metadata
-     * @param {string} scanId
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     */
-    getScanMetadata: async (
-      scanId: string,
-      options: RawAxiosRequestConfig = {},
-    ): Promise<RequestArgs> => {
-      // verify required parameter 'scanId' is not null or undefined
-      assertParamExists("getScanMetadata", "scanId", scanId);
-      const localVarPath = `/scans/{scan_id}/metadata`.replace(
-        `{${"scan_id"}}`,
-        encodeURIComponent(String(scanId)),
-      );
-      // use dummy base URL string because the URL constructor only accepts absolute URLs.
-      const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
-      let baseOptions;
-      if (configuration) {
-        baseOptions = configuration.baseOptions;
-      }
-
-      const localVarRequestOptions = {
-        method: "GET",
-        ...baseOptions,
-        ...options,
-      };
-      const localVarHeaderParameter = {} as any;
-      const localVarQueryParameter = {} as any;
-
-      // authentication bearerAuth required
-      // http bearer authentication required
-      await setBearerAuthToObject(localVarHeaderParameter, configuration);
-
-      setSearchParams(localVarUrlObj, localVarQueryParameter);
-      let headersFromBaseOptions =
-        baseOptions && baseOptions.headers ? baseOptions.headers : {};
-      localVarRequestOptions.headers = {
-        ...localVarHeaderParameter,
-        ...headersFromBaseOptions,
-        ...options.headers,
-      };
-
-      return {
-        url: toPathString(localVarUrlObj),
-        options: localVarRequestOptions,
-      };
-    },
-    /**
-     *
      * @summary List Cards
      * @param {number} [limit]
      * @param {string} [cursor]
@@ -909,6 +859,65 @@ export const CardScanApiAxiosParamCreator = function (
         ...headersFromBaseOptions,
         ...options.headers,
       };
+
+      return {
+        url: toPathString(localVarUrlObj),
+        options: localVarRequestOptions,
+      };
+    },
+    /**
+     *
+     * @summary Set Scan Metadata
+     * @param {string} scanId
+     * @param {object} [body]
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    setScanMetadata: async (
+      scanId: string,
+      body?: object,
+      options: RawAxiosRequestConfig = {},
+    ): Promise<RequestArgs> => {
+      // verify required parameter 'scanId' is not null or undefined
+      assertParamExists("setScanMetadata", "scanId", scanId);
+      const localVarPath = `/scans/{scan_id}/metadata`.replace(
+        `{${"scan_id"}}`,
+        encodeURIComponent(String(scanId)),
+      );
+      // use dummy base URL string because the URL constructor only accepts absolute URLs.
+      const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+      let baseOptions;
+      if (configuration) {
+        baseOptions = configuration.baseOptions;
+      }
+
+      const localVarRequestOptions = {
+        method: "POST",
+        ...baseOptions,
+        ...options,
+      };
+      const localVarHeaderParameter = {} as any;
+      const localVarQueryParameter = {} as any;
+
+      // authentication bearerAuth required
+      // http bearer authentication required
+      await setBearerAuthToObject(localVarHeaderParameter, configuration);
+
+      localVarHeaderParameter["Content-Type"] = "application/json";
+
+      setSearchParams(localVarUrlObj, localVarQueryParameter);
+      let headersFromBaseOptions =
+        baseOptions && baseOptions.headers ? baseOptions.headers : {};
+      localVarRequestOptions.headers = {
+        ...localVarHeaderParameter,
+        ...headersFromBaseOptions,
+        ...options.headers,
+      };
+      localVarRequestOptions.data = serializeDataIfNeeded(
+        body,
+        localVarRequestOptions,
+        configuration,
+      );
 
       return {
         url: toPathString(localVarUrlObj),
@@ -1327,34 +1336,6 @@ export const CardScanApiFp = function (configuration?: Configuration) {
     },
     /**
      *
-     * @summary Get Scan Metadata
-     * @param {string} scanId
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     */
-    async getScanMetadata(
-      scanId: string,
-      options?: RawAxiosRequestConfig,
-    ): Promise<
-      (axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>
-    > {
-      const localVarAxiosArgs = await localVarAxiosParamCreator.getScanMetadata(
-        scanId,
-        options,
-      );
-      const index = configuration?.serverIndex ?? 0;
-      const operationBasePath =
-        operationServerMap["CardScanApi.getScanMetadata"]?.[index]?.url;
-      return (axios, basePath) =>
-        createRequestFunction(
-          localVarAxiosArgs,
-          globalAxios,
-          BASE_PATH,
-          configuration,
-        )(axios, operationBasePath || basePath);
-    },
-    /**
-     *
      * @summary List Cards
      * @param {number} [limit]
      * @param {string} [cursor]
@@ -1450,6 +1431,37 @@ export const CardScanApiFp = function (configuration?: Configuration) {
       const index = configuration?.serverIndex ?? 0;
       const operationBasePath =
         operationServerMap["CardScanApi.searchCards"]?.[index]?.url;
+      return (axios, basePath) =>
+        createRequestFunction(
+          localVarAxiosArgs,
+          globalAxios,
+          BASE_PATH,
+          configuration,
+        )(axios, operationBasePath || basePath);
+    },
+    /**
+     *
+     * @summary Set Scan Metadata
+     * @param {string} scanId
+     * @param {object} [body]
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    async setScanMetadata(
+      scanId: string,
+      body?: object,
+      options?: RawAxiosRequestConfig,
+    ): Promise<
+      (axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>
+    > {
+      const localVarAxiosArgs = await localVarAxiosParamCreator.setScanMetadata(
+        scanId,
+        body,
+        options,
+      );
+      const index = configuration?.serverIndex ?? 0;
+      const operationBasePath =
+        operationServerMap["CardScanApi.setScanMetadata"]?.[index]?.url;
       return (axios, basePath) =>
         createRequestFunction(
           localVarAxiosArgs,
@@ -1677,18 +1689,6 @@ export const CardScanApiFactory = function (
     },
     /**
      *
-     * @summary Get Scan Metadata
-     * @param {string} scanId
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     */
-    getScanMetadata(scanId: string, options?: any): AxiosPromise<void> {
-      return localVarFp
-        .getScanMetadata(scanId, options)
-        .then((request) => request(axios, basePath));
-    },
-    /**
-     *
      * @summary List Cards
      * @param {number} [limit]
      * @param {string} [cursor]
@@ -1738,6 +1738,23 @@ export const CardScanApiFactory = function (
     ): AxiosPromise<SearchCards200Response> {
       return localVarFp
         .searchCards(query, limit, cursor, options)
+        .then((request) => request(axios, basePath));
+    },
+    /**
+     *
+     * @summary Set Scan Metadata
+     * @param {string} scanId
+     * @param {object} [body]
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    setScanMetadata(
+      scanId: string,
+      body?: object,
+      options?: any,
+    ): AxiosPromise<void> {
+      return localVarFp
+        .setScanMetadata(scanId, body, options)
         .then((request) => request(axios, basePath));
     },
     /**
@@ -2333,20 +2350,6 @@ export class CardScanApi extends BaseAPI {
 
   /**
    *
-   * @summary Get Scan Metadata
-   * @param {string} scanId
-   * @param {*} [options] Override http request option.
-   * @throws {RequiredError}
-   * @memberof CardScanApi
-   */
-  public getScanMetadata(scanId: string, options?: RawAxiosRequestConfig) {
-    return CardScanApiFp(this.configuration)
-      .getScanMetadata(scanId, options)
-      .then((request) => request(this.axios, this.basePath));
-  }
-
-  /**
-   *
    * @summary List Cards
    * @param {number} [limit]
    * @param {string} [cursor]
@@ -2401,6 +2404,25 @@ export class CardScanApi extends BaseAPI {
   ) {
     return CardScanApiFp(this.configuration)
       .searchCards(query, limit, cursor, options)
+      .then((request) => request(this.axios, this.basePath));
+  }
+
+  /**
+   *
+   * @summary Set Scan Metadata
+   * @param {string} scanId
+   * @param {object} [body]
+   * @param {*} [options] Override http request option.
+   * @throws {RequiredError}
+   * @memberof CardScanApi
+   */
+  public setScanMetadata(
+    scanId: string,
+    body?: object,
+    options?: RawAxiosRequestConfig,
+  ) {
+    return CardScanApiFp(this.configuration)
+      .setScanMetadata(scanId, body, options)
       .then((request) => request(this.axios, this.basePath));
   }
 
