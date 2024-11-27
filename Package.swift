c49dd7f6c7e6857d5caf1b12cffff5551caace36
Nick Lane-Smith <nlanesmith@gmail.com>
2024-11-27 14:18:06 -0800

    adding binary compatibility....

--- Package.swift
+++ Package.swift
@@ -26,8 +26,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CardScanClient",
-            dependencies: ["AnyCodable", ],
-            path: "clients/cardscan-swift/CardScanClient/Classes"
+            dependencies: ["AnyCodable"],
+            path: "clients/cardscan-swift/CardScanClient/Classes",
+            swiftSettings: [
+                .unsafeFlags(["-enable-library-evolution"], .when(configuration: .release))
+            ]
         ),
     ]
 )
